96c4f56839ef72399c30438f5f8b86d345ca5e8e
Brian Laub <blaub@palantir.com>
2025-02-10 13:19:33 -0500

    add DeadlineExpiredException
    Adds a base exception type DeadlineExpiredException, as well as two
    subtypes External and Internal.
    
    A future change will modify the library to throw one of these exception
    types when a deadline is exceeded, but for now the types are added to
    the public API to allow consumers to adopt logic to handle each
    exception type gracefully if necessary.

---
+++ deadlines-api/src/main/java/com/palantir/deadlines/api/DeadlineExpiredException.java
@@ -0,0 +1,83 @@
+/*
+ * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.deadlines.api;
+
+import com.palantir.logsafe.Arg;
+import com.palantir.logsafe.Safe;
+import com.palantir.logsafe.SafeLoggable;
+import java.util.List;
+
+/**
+ * Indicates that a deadline has expired.
+ */
+public abstract sealed class DeadlineExpiredException extends RuntimeException implements SafeLoggable {
+    protected static final List<Arg<?>> EMPTY_ARGS = List.of();
+
+    private DeadlineExpiredException(String message) {
+        super(message);
+    }
+
+    public static External external() {
+        return new External();
+    }
+
+    public static Internal internal() {
+        return new Internal();
+    }
+
+    /**
+     * Indicates that a deadline has expired due to a server being unable to meet an externally provided deadline.
+     */
+    public static final class External extends DeadlineExpiredException implements SafeLoggable {
+        private static final String MESSAGE = "An externally provided deadline for completing work has expired.";
+
+        private External() {
+            super(MESSAGE);
+        }
+
+        @Override
+        public @Safe String getLogMessage() {
+            return MESSAGE;
+        }
+
+        @Override
+        public List<Arg<?>> getArgs() {
+            return EMPTY_ARGS;
+        }
+    }
+
+    /**
+     * Indicates that a deadline has expired due to a server being unable to meet an internally-imposed deadline.
+     */
+    public static final class Internal extends DeadlineExpiredException implements SafeLoggable {
+        private static final String MESSAGE = "An internal deadline for completing work has expired.";
+
+        private Internal() {
+            super(MESSAGE);
+        }
+
+        @Override
+        public @Safe String getLogMessage() {
+            return MESSAGE;
+        }
+
+        @Override
+        public List<Arg<?>> getArgs() {
+            return EMPTY_ARGS;
+        }
+    }
+}
