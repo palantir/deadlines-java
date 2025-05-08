9dfa707a908a05df8d6537361df53304fa24c48e
Brian Laub <blaub@palantir.com>
2025-05-08 09:16:03 -0400

    make http header name constants public

--- deadlines/src/main/java/com/palantir/deadlines/DeadlinesHttpHeaders.java
+++ deadlines/src/main/java/com/palantir/deadlines/DeadlinesHttpHeaders.java
@@ -16,8 +16,32 @@
 
 package com.palantir.deadlines;
 
-interface DeadlinesHttpHeaders {
+/**
+ * Defines HTTP header names used by deadlines.
+ *
+ * When sending or parsing deadline metadata on HTTP requests, consumers typically do not need
+ * to interact with the constants in this class directly. Instead, use {@link Deadlines#encodeToRequest}
+ * and {@link Deadlines#parseFromRequest} with a supplied {@link Deadlines.RequestEncodingAdapter} to encode
+ * or decode header values from an HTTP request implementation type.
+ */
+public interface DeadlinesHttpHeaders {
+    /**
+     * Expect-Within is a client-provided deadline value, specified in decimal number of seconds.
+     *
+     * It indicates to a server that client expects a response within the given time, and may abort the request
+     * after that period of time.
+     */
     String EXPECT_WITHIN = "Expect-Within";
+
+    /**
+     * Expect-Within-Enforced is a boolean that will be set to "true" when deadline enforcement is requested.
+     */
     String EXPECT_WITHIN_ENFORCED = "Expect-Within-Enforced";
+
+    /**
+     * Deadline-Expired-Reason is a string indicating the reason for a deadline expiration (e.g. external vs internal).
+     *
+     * See also {@link DeadlineExpiredReasons}.
+     */
     String DEADLINE_EXPIRED_REASON = "Deadline-Expired-Reason";
 }
